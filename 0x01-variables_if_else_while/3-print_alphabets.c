35407d3ad4440040ea0df78afe4894beb3dc378d
Sammiefazzie <sammiefazzie@gmail.com>
2023-06-16 12:45:50 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	char b;
+
+	char e;
+
+	b = 'a';
+	e = 'A';
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	while
+		(e <= 'Z') {
+			putchar(e);
+			e++;
+		}
+	putchar('\n');
+	return (0);
+}
