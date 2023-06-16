b66674975fadff39db7ae49cad39b059538627b1
Sammiefazzie <sammiefazzie@gmail.com>
2023-06-16 10:36:30 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Program entry
+ * Return:0
+ */
 
+int main(void)
+{
+	char p;
+
+	p = 'a';
+	while
+		(p <= 'z') {
+			putchar(p);
+			p++;
+		}
+	putchar('\n');
+	return (0);
+}
