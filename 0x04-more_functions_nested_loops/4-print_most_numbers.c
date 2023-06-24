ccffcf02719ba7429c3a710da52fd2b9adfba74c
Sammiefazzie <sammiefazzie@gmail.com>
2023-06-23 20:36:37 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,17 @@
 #include "main.h"
 /**
- * print_most_numbers - Print numbers since 0 up to 9
- * descripion: print numbers excluding 2 and 4
- * Return: Numbers since 0 up to 9
+ * print_most_numbers - Print numbers since 0 to 9
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
-	int i = 0;
+	char i;
 
-	for (; i <= 0; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-	if (i == 2 || i ==4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(i + '0');
-	}
+	if (!(i == '2' || i == '4'))
+	_putchar(i);
 	}
 	_putchar('\n');
 }
