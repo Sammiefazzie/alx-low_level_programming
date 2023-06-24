1382113d1f8b3956596912600cc12e9e5f605a0e
Sammiefazzie <sammiefazzie@gmail.com>
2023-06-23 18:05:54 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints numbers 0 up to 9
+ * Return: The numbers since 0 up to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 && i <= 9; i++)
+	{
+	_putchar(i + '0');
+	}
+	_putchar('\n');
+}
