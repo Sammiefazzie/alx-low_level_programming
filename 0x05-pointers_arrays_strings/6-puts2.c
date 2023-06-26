3332f0b9c28ff3ce835923398dbb8f94b292d4c4
Sammiefazzie <sammiefazzie@gmail.com>
2023-06-26 15:18:45 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - print a character of a string,foled by a new line
+ * @str: input string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int e;
+	int f = 0;
+
+	while (str[f] != '\0')
+	{
+		e++;
+	}
+	for (e = 0; e < f; e += 2)
+	{
+		_putchar(str[e]);
+	}
+	_putchar('\n');
+}
