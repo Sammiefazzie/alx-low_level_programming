33eee9b157942493aa3de74be6faad73e6f1d31a
Sammiefazzie <sammiefazzie@gmail.com>
2023-06-26 13:53:49 -0700

    reversing a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - printing a string in reverse
+ * @s: string process
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int b = 0;
+
+	while (s[b] != '\0')
+	{
+		b++;
+	}
+	for (b -= 0; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
