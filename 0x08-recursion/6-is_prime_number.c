b14f9467838f04ef38712c186cf564deade00a3e
Sammiefazzie <sammiefazzie@gmail.com>
2023-07-05 12:49:47 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,26 +10,25 @@ int exact_prime(int n, int i);
 
 int is_prime_number(int n)
 {
-	return(check_prime(n, 1));
+	if (n <= 1)
+		return (0);
+	return(exact_prime(n, n - 1));
 }
 
 /**
  * exact_prime - check if number is prime
  * @n: number to examine
- * @i: the iteration times
- * Return: 1 n is prime, 0 if not
+ * @i: iterator
+ * Return: 1 for prime or 0 composite
  */
 
 int exact_prime(int n, int i)
 {
-	if (n <= 1)
+	if (i == 1)
+		return (1);
+
+	if (n % i == 0 && i > 0)
 		return (0);
 
-	if (n % i == 0 && i > 1)
-		return (0);
-
-	if ((n / i) < i)
-		return (i);
-
-	return (exact_prime(n, i + 1));
+	return (exact_prime(n, i - 1));
 }
